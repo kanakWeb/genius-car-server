18704b5dd890d16b9aaa5484959c2dbcee7a9782
kanakweb <kanakwebdev@gmail.com>
2022-04-25 03:54:37 +0600

    add to features server to client

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 require('dotenv').config();
-
+const jwt = require('jsonwebtoken');
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -28,6 +28,20 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCarService').collection('service')
+        const orderCollection=client.db('geniusCarService').collection('order')
+       
+        //Auth 
+        app.post('/login',async(req,res)=>{
+            const user=req.body;
+            const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+                expiresIn:'1d'
+            })
+            res.send({accessToken})
+        })
+        
+        
+        
+        //services Api
         app.get('/service', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
@@ -56,6 +70,21 @@ async function run() {
             const id=req.params.id;
             const query={_id:ObjectId(id)}
             const result=await serviceCollection.deleteOne(query)
+            res.send(result)
+        })
+        //order collection api
+
+        app.get('/orders',async(req,res)=>{
+            const email=req.query.email
+            const query={email:email}
+            const cursor=orderCollection.find(query)
+            const orders=await cursor.toArray()
+            res.send(orders)
+        })
+
+        app.post('/order',async(req,res)=>{
+            const order=req.body
+            const result= await orderCollection.insertOne(order)
             res.send(result)
         })
 
