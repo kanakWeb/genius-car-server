cb0fcd1df9d6f62a87245743ea09dccb5e131b6b
kanakweb <kanakwebdev@gmail.com>
2022-04-26 02:49:39 +0600

    sarver site token pass

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ app.use(cors())
 app.use(express.json())
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (! authHeader) {
+        return res.status(401).send({message: 'Unauthorize access'})
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({message: 'Forbidden access'})
+        }
+
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+
+
+}
+
 const uri = `mongodb+srv://${
     process.env.DB_USER
 }:${
@@ -28,20 +47,17 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCarService').collection('service')
-        const orderCollection=client.db('geniusCarService').collection('order')
-       
-        //Auth 
-        app.post('/login',async(req,res)=>{
-            const user=req.body;
-            const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
-                expiresIn:'1d'
-            })
+        const orderCollection = client.db('geniusCarService').collection('order')
+
+        // Auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
             res.send({accessToken})
         })
-        
-        
-        
-        //services Api
+
+
+        // services Api
         app.get('/service', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
@@ -49,42 +65,50 @@ async function run() {
             res.send(services)
         });
 
-        app.get("/service/:id",async(req,res)=>{
-            const id=req.params.id;
-            const query={_id: ObjectId(id)}
-            const service= await serviceCollection.findOne(query)
+        app.get("/service/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: ObjectId(id)
+            }
+            const service = await serviceCollection.findOne(query)
             res.send(service)
         })
 
 
-        //POST
-        app.post('/service',async(req,res)=>{
-            const newService=req.body
+        // POST
+        app.post('/service', async (req, res) => {
+            const newService = req.body
 
-            const result= await serviceCollection.insertOne(newService)
+            const result = await serviceCollection.insertOne(newService)
             res.send(result);
         })
 
-        //delete
-        app.delete('/service/:id',async(req,res)=>{
-            const id=req.params.id;
-            const query={_id:ObjectId(id)}
-            const result=await serviceCollection.deleteOne(query)
+        // delete
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: ObjectId(id)
+            }
+            const result = await serviceCollection.deleteOne(query)
             res.send(result)
         })
-        //order collection api
+        // order collection api
 
-        app.get('/orders',async(req,res)=>{
-            const email=req.query.email
-            const query={email:email}
-            const cursor=orderCollection.find(query)
-            const orders=await cursor.toArray()
+        app.get('/order', verifyJWT, async (req, res) => {
+
+            const email = req.query.email
+            const query = {
+                email: email
+            }
+            const cursor = orderCollection.find(query)
+            const orders = await cursor.toArray()
             res.send(orders)
+
         })
 
-        app.post('/order',async(req,res)=>{
-            const order=req.body
-            const result= await orderCollection.insertOne(order)
+        app.post('/order', async (req, res) => {
+            const order = req.body
+            const result = await orderCollection.insertOne(order)
             res.send(result)
         })
 
